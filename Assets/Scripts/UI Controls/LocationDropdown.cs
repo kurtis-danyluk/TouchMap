9efb46ea10591d505bf99407ad034a1b93966e4a
Wesley Willett <wesley.willett@ucalgary.ca>
2018-06-11 17:49:27 -0600

    Added Monte Vista location

--- Assets/Scripts/UI Controls/LocationDropdown.cs
+++ Assets/Scripts/UI Controls/LocationDropdown.cs
@@ -27,17 +27,20 @@ public class LocationDropdown : MonoBehaviour {
         {
             case 0:
                 map.ChangeTile(51.2894f, -115.4f, 11); //Canmore
-                break;
-            case 1:
+				break;
+			case 1:
+				map.ChangeTile(37.7f, -106.35f, 11); //Monte Vista
+				break;
+            case 2:
                 map.ChangeTile(28.1000f, 86.8658f, 11,'a',0,0,4); //Everest
                 break;
-            case 2:
+            case 3:
                 map.ChangeTile(37.048f, -110.122f, 13, 'a', -3, -2, 1); //Monument valley
                 break;
-            case 3:
+            case 4:
                 map.ChangeTile(51.1345f, 1.3573f, 15, 'a', -3, -1, 4); //Cliffs of Dover
                 break;
-            case 4:
+            case 5:
                 map.ChangeTile(36.2144f, -113.0565f, 13,'a', -2, -2); //Grand Canyon
                 break;
         }
